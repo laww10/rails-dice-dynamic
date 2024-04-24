43299a5c089c74d880e6d8ccbc22f341dd072bac
laww10 <114757219+laww10@users.noreply.github.com>
2024-04-24 00:14:13 +0000

    push

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,20 @@
+class PagesController < ApplicationController 
+  def home
+    render({ :template => "display_templates/home"})
+  end
+
+  def input
+    @num_dice = params.fetch("number_of_dice").to_i
+    @sides = params.fetch("how_many_sides").to_i
+    @rolls = []
+
+    @num_dice.times do
+      die = rand(1..@sides)
+
+      @rolls.push(die)
+    end
+    
+    render({ :template => "display_templates/inputs"})
+  end
+
+end
